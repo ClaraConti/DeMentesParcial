e7e3af96914b1039a8c5777252ab94140d06e2d2
Estudiante <estudiante@UCUXA40225.continental.edu.pe>
2024-05-07 14:39:53 -0500

    Proyecto

---
+++ CapaEntidad/Carrera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Carrera
+    {
+            public String CodCarrera
+            { get; set; }
+            public String Carreras
+            { get; set; }
+        
+    }
+}
---
+++ CapaEntidad/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Usuario
+    {
+        public String Usuarios
+        { get; set; }
+        public String Contrasena
+        { get; set; }
+        
+    }
+}
